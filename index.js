be0849848fd879256fae9b556388cb762d06c4b7
Spazen's PC <prakashamtara07@gmail.com>
2020-05-07 14:16:19 +0530

    completed

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const todoRoutes = require("./routes/todos");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname+"/views"));
 
 app.get("/", (req,res) => {
-  res.json({message: "Hello From Express!!"});
+  res.sendFile("index.html");
 });
+
+app.use("/api/todos", todoRoutes);
 
 app.listen(port, () => {
   console.log("Server is Up & Running!!");
